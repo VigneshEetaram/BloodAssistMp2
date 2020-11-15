e98eac8c2453d0a2ec8ce5d804ac522484bbb597
vignesheetaram <vignesh.eetaram@gmail.com>
2020-11-14 22:18:47 -0500

    Register and Login Page and Fire Base and pages

--- app/src/main/java/com/example/bloodassistmp2/HomePage.java
+++ app/src/main/java/com/example/bloodassistmp2/HomePage.java
@@ -5,15 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends AppCompatActivity {
+    private Button buttonMain, donarBtn,recBtn,reqBtn;
+    private Button profileBtn,settingsBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        donarBtn = findViewById(R.id.donar);
+        donarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), donar.class));
+            }
+        });
+
+        recBtn = findViewById(R.id.receiver);
+        recBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Receiver.class));
+            }
+        });
+
+        reqBtn = findViewById(R.id.request);
+        reqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Requests.class));
+            }
+        });
     }
 
     public void logout(View view) {
