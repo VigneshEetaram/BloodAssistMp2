88912006a9ecffb8fb7751fd6c99a60ec25f5e06
vignesheetaram <vignesh.eetaram@gmail.com>
2020-11-21 17:10:38 -0500

    Register, Login Page, Fire Base, pages, splash animation, background.

--- app/src/main/java/com/example/bloodassistmp2/Login.java
+++ app/src/main/java/com/example/bloodassistmp2/Login.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -28,6 +29,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
         uEmail = findViewById(R.id.email);
