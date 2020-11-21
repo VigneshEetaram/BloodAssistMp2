c817484d0dfbb2e41da837ae406b82e0560cebfb
vignesheetaram <vignesh.eetaram@gmail.com>
2020-11-21 16:17:43 -0500

    Register and Login Page and Fire Base and pages and splash animation

--- app/src/main/java/com/example/bloodassistmp2/MainActivity.java
+++ app/src/main/java/com/example/bloodassistmp2/MainActivity.java
@@ -5,13 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Animation topanim;
     private static int SPLASH_TIME_OUT = 4000;
+    ImageView introimg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        introimg=findViewById(R.id.imageView);
+        introimg.setAnimation(topanim);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -20,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         },SPLASH_TIME_OUT);
+
 
     }
 }
