bc756b21a713acb42182d6a54e92702b2ea44825
vignesheetaram <vignesh.eetaram@gmail.com>
2020-11-21 18:57:56 -0500

    Register, Login Page, Fire Base, pages, splash animation, background, alignment.

--- app/src/main/java/com/example/bloodassistmp2/MainActivity.java
+++ app/src/main/java/com/example/bloodassistmp2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -17,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
         topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
         introimg=findViewById(R.id.imageView);
